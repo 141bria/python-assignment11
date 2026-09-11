07e218bbbb585527e7c6825e6be8f79d5822bac1
Brii <141.brii@gmail.com>
2026-09-11 17:08:33 -0500

    hw11

--- assignment11/plotly_wind.py
+++ assignment11/plotly_wind.py
@@ -4,3 +4,7 @@ import plotly.data as pldata
 df = pldata.wind(return_type='pandas')
 print(df.head(10))
 print(df.tail(10))
+float()
+df["strength"] = df["strength"].str.replace(r"-.*|\+","",regex=True).astype(float)
+fig = px.scatter(df, x= 'strength',y='frequency',color='direction')
+fig.write_html("wind.html",auto_open=True)
